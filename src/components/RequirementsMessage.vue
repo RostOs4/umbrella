a024cc377f477a3d041563498535e23648266d2a
RostOS <124255484+RostOs4@users.noreply.github.com>
2024-09-16 13:05:26 +0300

    Update RequirementsMessage.vue

--- src/components/RequirementsMessage.vue
+++ src/components/RequirementsMessage.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from '.vue';
 export default defineComponent({
   name: "RequirementsMessage",
   props: {
